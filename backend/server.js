700f4e230ef6be91a41170aee1258e2222ab1cec
Samson Akanbi <samson.akanbi@outlook.com>
2021-08-01 23:07:46 +0100

    Created the user model.

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true})
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
