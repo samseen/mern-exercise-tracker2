cf68343561ec0ed69424d53585480d9dfa34e3da
Samson Akanbi <samson.akanbi@outlook.com>
2021-08-04 20:13:06 +0100

    Added the users from the front end.

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
     constructor(props) {
@@ -26,6 +27,9 @@ export default class CreateUsers extends Component {
         }
 
         console.log(user);
+
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
 
         this.setState({
             username: ''
